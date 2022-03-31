74c81f23cdc06bb55feb1ae9a7f42ffbc1aed36e
David <dsem562@gmail.com>
2022-03-31 05:10:32 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+function findMatching(array, string) {
+    const drivers = array.filter(checkName);
+        function checkName(name) {
+            if (name.toUpperCase() === string.toUpperCase()) {
+                return name;
+            }
+        }
+    return drivers;
+}
+
+function fuzzyMatch(array, string) {
+    const drivers = array.filter(checkName);
+        function checkName(name) {
+            const firstNameLetter1 = name.charAt(0);
+            const firstNameLetter2 = string.charAt(0);
+            if (firstNameLetter1 === firstNameLetter2) {
+                return name;
+            }
+        }
+    return drivers;
+}
+
+function matchName(objects, string) {
+    const drivers = objects.filter(checkDriver);
+        function checkDriver(driver) {            
+            if (driver.name === string) {
+                return driver;
+            }
+        }
+    return drivers;
+}
